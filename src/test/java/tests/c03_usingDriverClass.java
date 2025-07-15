8fecdfa23a729ea65826e4ec466b9f260a21ee98
cgurcay <cgurcay7@gmail.com>
2025-07-15 16:59:28 +0300

    test

--- src/test/java/tests/c03_usingDriverClass.java
+++ src/test/java/tests/c03_usingDriverClass.java
@@ -1,2 +1,35 @@
-package tests;public class c03_usingDriverClass {
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class c03_usingDriverClass {
+
+    @Test
+    public void test01(){
+        Driver.getDriver().get("https://testotomasyonu.com");
+
+        String expectedUrl = "testotomasyonu";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+
+        Assert.assertTrue(actualUrl.contains(expectedUrl));
+
+        Driver.quitDriver();
+    }
+
+    @Test
+    public void test02(){
+
+        Driver.getDriver().get("https://wisequarter.com");
+
+        String expectedTitle = "Wise Quarter";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+        Driver.quitDriver();
+    }
+
 }
